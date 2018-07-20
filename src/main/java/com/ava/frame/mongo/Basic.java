64b4f71d322c50d9ae3f35b7d6fe20700c8c10e4
redredava <zhyx2014@yeah.net>
2018-07-20 10:07:21 +0800

    init

--- src/main/java/com/ava/frame/mongo/Basic.java
+++ src/main/java/com/ava/frame/mongo/Basic.java
@@ -10,6 +10,7 @@ import java.util.Date;
 
 public class Basic implements Serializable {
 
+
     private Long createAt = new Date().getTime() / 1000;
 
     private Long updateAt = new Date().getTime() / 1000;
